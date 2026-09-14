e5c0592cb29c125914db724b5528a0815b781b10
Yuniek Anand <yuniekanand@gmail.com>
2026-09-14 16:17:48 +0530

    835th problem solved of Image Overlap

---
+++ 000835.imageOverlap.py
@@ -0,0 +1,26 @@
+"835. Image Overlap"
+from typing import List
+class Solution:
+    def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
+        from collections import Counter
+        n = len(img1)
+        ones1 = []
+        ones2 = []
+
+        for r in range(n):
+            for c in range(n):
+                if img1[r][c] == 1:
+                    ones1.append((r, c))
+                if img2[r][c] == 1:
+                    ones2.append((r, c))
+
+        # Count how many pairs produce the same translation.
+        translations = Counter()
+
+        for r1, c1 in ones1:
+            for r2, c2 in ones2:
+                dr = r2 - r1
+                dc = c2 - c1
+                translations[(dr, dc)] += 1
+
+        return max(translations.values(), default=0)
