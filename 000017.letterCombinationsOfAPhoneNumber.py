0e91d0ad368572f9367bd2a9656d753a6a2bbf86
Yuniek Anand <yuniekanand@gmail.com>
2026-09-14 16:18:31 +0530

    17th problem solved of Letter Combinations of a Phone Number

---
+++ 000017.letterCombinationsOfAPhoneNumber.py
@@ -0,0 +1,32 @@
+"17. Letter Combinations of a Phone Number"
+from typing import List
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+
+        mapping = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+
+        result = []
+
+        def backtrack(index, current):
+            # We've used every digit
+            if index == len(digits):
+                result.append(current)
+                return
+
+            # Try every letter for the current digit
+            for letter in mapping[digits[index]]:
+                backtrack(index + 1, current + letter)
+
+        backtrack(0, "")
+        return result
